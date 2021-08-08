c0be343c68bc3df267732776916a6f08c418449d
Ahmet Aksungur <55207238+Ahmetaksungur@users.noreply.github.com>
2021-08-08 17:34:35 +0300

    Update fixed.ts

--- database/fixed.ts
+++ database/fixed.ts
@@ -20,19 +20,6 @@ async function fixed() {
         last_login VARCHAR(2000) NOT NULL
     ) CHARACTER SET utf8`);
 
-  await client.execute(`CREATE TABLE IF NOT EXISTS users (
-        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        user_name VARCHAR(64) NOT NULL,
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
-        email VARCHAR(64) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        user_type VARCHAR(50) NOT NULL,
-        user_status VARCHAR(50) NOT NULL,
-        create_date VARCHAR(500) NOT NULL,
-        last_login VARCHAR(2000) NOT NULL
-    ) CHARACTER SET utf8`);
-
   await client.execute(`CREATE TABLE IF NOT EXISTS user_meta (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
         user_id INT(11) NOT NULL,
